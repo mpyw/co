6c5a2f0cd2e98f678151da7188a6be8225bdec69
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-02-29 04:44:26 +0900

    Fixed typos

--- src/Co.php
+++ src/Co.php
@@ -3,7 +3,7 @@
 namespace mpyw\Co;
 
 /**
- * Asynchronus cURL executor simply based on resource and Generator.
+ * Asynchronous cURL executor simply based on resource and Generator.
  * http://github.com/mpyw/co
  *
  * @author mpyw
