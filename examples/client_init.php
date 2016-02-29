c32f6474751482c1136c918f2ecd4c5f348ced26
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-02-29 16:51:48 +0900

    Added example to debug function

--- examples/client_init.php
+++ examples/client_init.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 set_time_limit(0);
 
+// REST API
 function curl($path, array $q = array()) {
     $ch = curl_init();
     curl_setopt_array($ch, array(
@@ -12,6 +13,8 @@ function curl($path, array $q = array()) {
     ));
     return $ch;
 }
+
+// Streaming API
 function curl_streaming($path, $callback, array $q = array()) {
     $ch = curl_init();
     curl_setopt_array($ch, array(
@@ -22,4 +25,22 @@ function curl_streaming($path, $callback, array $q = array()) {
         },
     ));
     return $ch;
+}
+
+// Just for debugging
+function co_dump() {
+    static $reg_values = '@^  \["values":"mpyw\\\\Co\\\\Co":private\]=>\n  array\(\d+?\) {\n(.*?)\n  \}$@ms';
+    static $reg_gens = '@\["(\w+)"\]=>\s+object\(Generator\)#(\d+)@';
+    $r = new ReflectionProperty('mpyw\Co\Co', 'self');
+    $r->setAccessible(true);
+    ob_start();
+    var_dump($r->getValue());
+    $dumped = ob_get_clean();
+    if (preg_match($reg_values, $dumped, $m_value)) {
+        preg_match_all($reg_gens, $m_value[1], $m_gens, PREG_SET_ORDER);
+        foreach ($m_gens as $m_gen) {
+            $dumped = str_replace($m_gen[1], "Generator id #$m_gen[2]", $dumped);
+        }
+    }
+    echo $dumped;
 }
